3ef17fe10bc4da112ca231ec9f68fad7ec7214ee
justindavisa <justindavisa@gmail.com>
2019-02-15 13:01:40 -0500

    Reasonable style, moving to pagination

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function Character(props) {
     return (
-        <div className="char-card">
-            <h1>{props.char.name}</h1>
-            <div>
-                <strong>Race:</strong> {props.char.race}
-                <strong>Skin Color:</strong> {props.char.skin_color}
-                <strong>Hair Color:</strong> {props.char.hair_color}
-                <strong>Eye Color:</strong> {props.char.eye_color}
-                <strong>Height:</strong> {props.char.height}
+        <div className="card-list">
+            <div className="char-card">
+                <h1>{props.char.name}</h1>
+                <div className="phys-details">
+                    <strong>Gender:</strong> {props.char.gender}
+                    <strong>Skin Color:</strong> {props.char.skin_color}
+                    <strong>Hair Color:</strong> {props.char.hair_color}
+                    <strong>Eye Color:</strong> {props.char.eye_color}
+                    <strong>Height:</strong> {props.char.height}
+                    <strong>Mass:</strong> {props.char.mass}
+                </div>
             </div>
         </div>
     );
