84c6ce9034f91871bbaeae01b93194da7ee13b87
justindavisa <justindavisa@gmail.com>
2019-02-15 12:47:30 -0500

    Much styling added, implementing flex for a nicer design, then stretch

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,9 +4,13 @@ function Character(props) {
     return (
         <div className="char-card">
             <h1>{props.char.name}</h1>
-            <p>
+            <div>
+                <strong>Race:</strong> {props.char.race}
+                <strong>Skin Color:</strong> {props.char.skin_color}
+                <strong>Hair Color:</strong> {props.char.hair_color}
                 <strong>Eye Color:</strong> {props.char.eye_color}
-            </p>
+                <strong>Height:</strong> {props.char.height}
+            </div>
         </div>
     );
 }
